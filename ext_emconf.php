a834194e17880abc34fed148dad67816bdfca4dc
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-09-13 16:21:17 +0200

    set version

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'thomas.rawiel@gmail.com',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '2.0.0',
+    'version' => '2.1.0',
     'constraints' => [
         'depends' => [
-            'seo' => '12.4.0-13.99.99',
+            'seo' => '12.4.0-12.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],

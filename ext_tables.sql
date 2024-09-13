7eb84f247cff8ea380002932283a51d3f088a3c1
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-09-13 13:35:52 +0200

    Remove unneccessary table definition - automatically created by TCA setup

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,15 +4,3 @@ CREATE TABLE pages
     `tx_hreflang_pages_pages_2`  int(11) unsigned DEFAULT '0' NOT NULL,
     `tx_hreflang_pages_xdefault` tinyint(4) unsigned DEFAULT '0' NOT NULL
 );
-
-CREATE TABLE tx_hreflang_pages_page_page_mm
-(
-    `uid_local`       int(11) DEFAULT '0' NOT NULL,
-    `uid_foreign`     int(11) DEFAULT '0' NOT NULL,
-    `sorting`         int(11) DEFAULT '0' NOT NULL,
-    `sorting_foreign` int(11) DEFAULT '0' NOT NULL,
-
-    KEY               uid_local_foreign (uid_local, uid_foreign),
-    KEY               uid_local (uid_local),
-    KEY               uid_foreign (uid_foreign)
-);

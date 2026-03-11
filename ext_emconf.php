5cef015a5e34a7dd4e2f48574e7a0a748507aef4
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2026-03-11 18:30:54 +0100

    [task] prepare v14

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,8 +10,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-13.4.99',
-            'seo' => '13.4.0-13.99.99',
+            'typo3' => '13.4.0-14.99.99',
+            'seo' => '13.4.0-14.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
